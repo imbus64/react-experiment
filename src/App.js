be0a023734c7cd72a66c8df692d209c8d6139afc
Imbus <imbus64@protonmail.com>
2022-12-03 05:21:02 +0100

    Experimenting

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useState, useRef, useEffect } from 'react';
+import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid';
+import "./style.css"
+
+const LOCAL_STORAGE_KEY = "todo.todos";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef()
+
+  useEffect(() => {
+    const stored = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (stored.length > 0) {
+      // console.log("READ", stored);
+      setTodos(stored);
+      console.log("LOADED: ", todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+    console.log("Writing")
+  }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos];
+    const todo = newTodos.find(todo => todo.id === id);
+    todo.complete = !todo.complete;
+    setTodos(newTodos)
+  }
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name === '') return;
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
+    })
+    // console.log(name);
+    todoNameRef.current.value = null;
+  }
+
+  function handleClear(e) {
+    setTodos(todos.filter(todo => !todo.complete));
+  }
+
+  return (
+    <>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add</button>
+      <button onClick={handleClear}>Clear Complete</button>
+      <div>{todos.filter(todo => !todo.complete).length} tasks left</div>
+    </>
+  );
+}
+
+export default App;
